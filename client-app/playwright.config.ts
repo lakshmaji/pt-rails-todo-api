8aa29f4ec3f4b8065484b50b3b121e2dbb341090
Lakshmaji <lakshmaji@users.noreply.github.com>
2024-06-29 18:22:14 +0530

    ci: :green_heart: Update base url for e2e

--- client-app/playwright.config.ts
+++ client-app/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3001",
     // storageState: "playwright/.auth/storageState.json",
   },
 
