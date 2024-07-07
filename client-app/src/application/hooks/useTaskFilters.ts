46daf271061b430f52f7fee2ce5977e27e2da7fe
Lakshmaji <lakshmaji@users.noreply.github.com>
2024-07-07 18:52:38 +0530

    fix: :bug: Handles invalid status and page params
    Handle human made errors such as modified url with incorrect status and page number

--- client-app/src/application/hooks/useTaskFilters.ts
+++ client-app/src/application/hooks/useTaskFilters.ts
@@ -1,12 +1,23 @@
 import { TaskStatus } from "../../domain/models/Task";
 import { URLSearchParamsInit, useSearchParams } from "react-router-dom";
 
+const toTaskStatus = (status: string | null): TaskStatus => {
+  if (status && Object.values(TaskStatus).includes(status as TaskStatus)) {
+    return status as TaskStatus;
+  }
+  return undefined as unknown as TaskStatus;
+};
+
+const toPage = (page: string | null): number => {
+  // handles NaN
+  return parseInt(page || "1", 10) ?? 1;
+};
+
 export const useTaskFilters = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const statusFilter =
-    (searchParams.get("statusFilter") as TaskStatus) ?? undefined;
-  const page = parseInt(searchParams.get("page") || "1", 10);
+  const statusFilter = toTaskStatus(searchParams.get("statusFilter"));
+  const page = toPage(searchParams.get("page"));
 
   const changeSearchParams = <T extends Record<string, string>>(props: T) => {
     setSearchParams({
